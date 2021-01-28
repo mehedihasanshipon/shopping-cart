5ae4c3cc358bdf92b13d011e1d89e65aa430aed1
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-01-28 14:05:34 +0600

    Shopping cart

--- script.js
+++ script.js
@@ -2,31 +2,27 @@
 let phonePriceNumber = getInputNumber("phonePrice");
 let casePriceNumber = getInputNumber("caseprice");
 
-
 //Phone Plus button even handler
 const plusBtn = document.getElementById("plusBtn");
 plusBtn.addEventListener('click',function(){
-    // const phoneItem = document.getElementById('phoneInputValue').value;
-    // let phoneItemNumber = parseFloat(phoneItem);
-    // let phoneItems = phoneItemNumber + 1;
-
     let phoneQuantity = incrementFunction('phoneInputValue');
     document.getElementById('phoneInputValue').value = phoneQuantity;
     // Phone price
     let totalPrice = phoneQuantity * phonePriceNumber;
     document.getElementById("phonePrice").innerText = totalPrice;
+    // subtotal price
+    // updateSpanText("subtotal",phonePriceNumber)
+    test();
 })
 
 const minusBtn = document.getElementById('minusBtn');
 minusBtn.addEventListener('click',function(){
-    // const phoneItem = document.getElementById('phoneInputValue').value;
-    // let phoneItemNumber = parseFloat(phoneItem);
-    // let phoneItems = phoneItemNumber - 1;
     let phoneQuantity = decrementFunction('phoneInputValue');
     document.getElementById('phoneInputValue').value = phoneQuantity;
     // Phone price
     let totalPrice = phoneQuantity * phonePriceNumber;
     document.getElementById("phonePrice").innerText = totalPrice;
+    test();
 })
 
 // Case Plus button even handler
@@ -34,18 +30,20 @@ const casePlusBtn = document.getElementById("casePlusBtn");
 casePlusBtn.addEventListener('click',function(){
     let caseQuantity = incrementFunction('caseInputValue');
     document.getElementById('caseInputValue').value = caseQuantity;
-    // Phone price
+    // Case price
     let totalCasePrice = caseQuantity * casePriceNumber;
     document.getElementById("caseprice").innerText = totalCasePrice;
+    test();
 })
 // Case Minus button even handler
 const caseMinusBtn = document.getElementById('caseMinusBtn');
 caseMinusBtn.addEventListener('click',function(){
     let caseQuantity = decrementFunction('caseInputValue');
     document.getElementById('caseInputValue').value = caseQuantity;
-    // Phone price
+    // Case price
     let totalCasePrice = caseQuantity * casePriceNumber;
     document.getElementById("caseprice").innerText = totalCasePrice;
+    test()
 })
 
 // Get input number function 
@@ -59,14 +57,34 @@ function getInputNumber(id){
 function incrementFunction(id){
     const item = document.getElementById(id).value;
     let itemNumber = parseFloat(item);
-    let items = itemNumber + 1;
-    return items;
+    itemNumber++;
+    
+    return itemNumber;
 }
 
 // Quantity Decrement function
 function decrementFunction(id){
     const item = document.getElementById(id).value;
     let itemNumber = parseFloat(item);
-    let items = itemNumber - 1;
-    return items;
+    itemNumber --;
+    return itemNumber;
+}
+// Update span text
+// function updateSpanText(id,addedNumber){
+//     const current = document.getElementById(id).innerText;
+//     const currentNumber = parseFloat(current);
+//     console.log(currentNumber);
+//     const totalAmount = addedNumber + currentNumber;
+//     document.getElementById(id).innerText = totalAmount;
+// }
+
+function test(){
+    const phone = document.getElementById("phonePrice").innerText;
+    const phoneNumber = parseFloat(phone);
+    const casePrice = document.getElementById("caseprice").innerText;
+    const caseNumber = parseFloat(casePrice);
+
+    let total = phoneNumber + caseNumber;
+
+    document.getElementById("subtotal").innerText = total;
 }
